9d4d2046afdf482f3da4adb8c51868c492a2daea
Umberto <umbertopellegrino96@gmail.com>
2020-07-03 17:56:20 +0200

    Correzione function

--- js/jscript.js
+++ js/jscript.js
@@ -1,50 +1,44 @@
 $(document).ready(function () {
-  $("#right").click(function() {
-    next()
-  });
+  $("#right").click(next);
 
-  $("#left").click(function() {
-    prev()
-  });
+  $("#left").click(prev);
 
-  $(document).keydown(function(){
-    pressKey()
-  });
+  $(document).keydown(pressKey);
 
 });
 
 // -----functions----- //
   function next() {
-  var imgShow=$("#imgCollum img.show");
-  imgShow.removeClass("show");
+    var imgShow=$("#imgCollum img.show");
+    imgShow.removeClass("show");
 
-  var navShow=$("#nav div.show");
-  navShow.removeClass("show");
+    var navShow=$("#nav div.show");
+    navShow.removeClass("show");
 
-  if (imgShow.hasClass("last")) {
-    $("#imgCollum img.first").addClass("show");
-    $("#nav div.first").addClass("show");
-  } else {
-    imgShow.next().addClass("show");
-    navShow.next().addClass("show");
-  }
-}
+    if (imgShow.hasClass("last")) {
+      $("#imgCollum img.first").addClass("show");
+      $("#nav div.first").addClass("show");
+    } else {
+      imgShow.next().addClass("show");
+      navShow.next().addClass("show");
+    }
+  };
 
   function prev() {
-  var imgShow=$("#imgCollum img.show");
-  imgShow.removeClass("show");
+    var imgShow=$("#imgCollum img.show");
+    imgShow.removeClass("show");
 
-  var navShow=$("#nav div.show");
-  navShow.removeClass("show");
+    var navShow=$("#nav div.show");
+    navShow.removeClass("show");
 
-  if (imgShow.hasClass("first")) {
-    $("#imgCollum img.last").addClass("show");
-    $("#nav div.last").addClass("show");
-  } else {
-    imgShow.prev().addClass("show");
-    navShow.prev().addClass("show");
-  }
-}
+    if (imgShow.hasClass("first")) {
+      $("#imgCollum img.last").addClass("show");
+      $("#nav div.last").addClass("show");
+    } else {
+      imgShow.prev().addClass("show");
+      navShow.prev().addClass("show");
+    }
+  };
 
   function pressKey() {
     var key=event.which;
@@ -53,4 +47,4 @@ $(document).ready(function () {
     } else if (key==37) {
       prev()
     }
-  }
+  };

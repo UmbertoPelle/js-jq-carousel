7cb464117bd11aff6e03b2181707984f92f5fd22
Umberto <umbertopellegrino96@gmail.com>
2020-07-03 15:40:57 +0200

    functions

--- js/jscript.js
+++ js/jscript.js
@@ -1,23 +1,44 @@
 $(document).ready(function () {
   $("#right").click(function() {
-    var imgShow=$("#imgCollum img.show");
-    imgShow.removeClass("show");
-
-    if (imgShow.hasClass("last")) {
-      $("#imgCollum img.first").addClass("show");
-    } else {
-      imgShow.next().addClass("show");
-    }
+    next()
   });
 
   $("#left").click(function() {
-    var imgShow=$("#imgCollum img.show");
-    imgShow.removeClass("show");
-
-    if (imgShow.hasClass("first")) {
-      $("#imgCollum img.last").addClass("show");
-    } else {
-      imgShow.prev().addClass("show");
-    }
+    prev()
   });
 });
+
+
+
+// -----funcions----- //
+function next() {
+  var imgShow=$("#imgCollum img.show");
+  imgShow.removeClass("show");
+
+  var navShow=$("#nav div.show");
+  navShow.removeClass("show");
+
+  if (imgShow.hasClass("last")) {
+    $("#imgCollum img.first").addClass("show");
+    $("#nav div.first").addClass("show");
+  } else {
+    imgShow.next().addClass("show");
+    navShow.next().addClass("show");
+  }
+}
+
+function prev() {
+  var imgShow=$("#imgCollum img.show");
+  imgShow.removeClass("show");
+
+  var navShow=$("#nav div.show");
+  navShow.removeClass("show");
+
+  if (imgShow.hasClass("first")) {
+    $("#imgCollum img.last").addClass("show");
+    $("#nav div.last").addClass("show");
+  } else {
+    imgShow.prev().addClass("show");
+    navShow.prev().addClass("show");
+  }
+}
